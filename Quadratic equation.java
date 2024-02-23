ef41dfc4660d1a3bfd761757d4c258d89ec8e256
Deekshith B <130782878+bdeekshith066@users.noreply.github.com>
2024-02-24 00:34:59 +0530

    Update and rename Quadratic equation to Quadratic equation.java

--- Quadratic equation.java
+++ Quadratic equation.java
@@ -1,10 +1,10 @@
-QUESTION -  Develop a Java program that prints all real solutions to the quadratic equation ax2+bx+c = 0. 
+/*QUESTION -  Develop a Java program that prints all real solutions to the quadratic equation ax2+bx+c = 0. 
             Read in a, b, c and use the quadratic formula. If the discriminate b2-4ac is negative, 
-            display a message stating that there are no real solutions.
+            display a message stating that there are no real solutions.*/
 
 
 
-                                              CODE
+                                              //CODE
 
 import java.util.Scanner;
 
@@ -58,7 +58,7 @@ class QuadraticMain {
 }
 
 
-                                                                    OUTPUT
+                                                 // OUTPUT
 
 1)
 Enter the coefficients of a, b, c
