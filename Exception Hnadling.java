3055e2cb7f62ada820cc5be68f7fbfe0b1bc0309
Deekshith B <130782878+bdeekshith066@users.noreply.github.com>
2024-02-23 23:45:02 +0530

    Update Exception Handling.java

--- Exception Hnadling.java
+++ Exception Hnadling.java
@@ -1,3 +1,11 @@
+                                                                            //Question
+/*Write a program that demonstrates handling of exceptions in inheritance tree. Create a base class called “Father” and derived class called “Son” which extends the base class.
+In Father class, implement a constructor which takes the age and throws the exception WrongAge( ) when the input age<0. In Son class, implement a constructor that cases 
+both father and son’s age and throws an exception if son’s age is >=father’s age.*/
+    
+
+                                                                                //Code
+
 import java.util.Scanner;
 class WrongAge extends Exception {
     public WrongAge(String message) {
